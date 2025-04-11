232ce54c311dffaeab860ed6573494225002d968
avkanavina <a.kanavina@skyeng.ru>
2025-04-11 18:55:42 +0300

    Fix style

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Course, Lesson
 
+from django.contrib import admin
+from .models import Lesson
+from django.utils.safestring import mark_safe
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'formatted_content')
+
+    def formatted_content(self, obj):
+        return mark_safe(obj.content.replace("\n", "<br>"))
+    formatted_content.short_description = 'Содержание'
+
+
 admin.site.register(Course)
 admin.site.register(Lesson)
