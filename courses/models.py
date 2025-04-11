a38460c246cbc11868edbf5705f00b471460f7ce
avkanavina <a.kanavina@skyeng.ru>
2025-04-11 19:17:33 +0300

    Fix style

--- courses/models.py
+++ courses/models.py
@@ -7,7 +7,7 @@ class Course(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='course_images/', null=True, blank=True)  # Добавляем поле для изображения
     duration = models.IntegerField(verbose_name='Продолжительность (в часах)')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена (в рублях)')
+    price = models.IntegerField(verbose_name='Цена (в рублях)')
 
     # def __init__(self, *args, **kwargs):
     #     super().__init__(args, kwargs)
@@ -21,6 +21,7 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
+    duration = models.IntegerField(verbose_name='Продолжительность (в неделях)')
 
     def __str__(self):
         return self.title
