0c4db81d955679f8475d9d738a1b1d096379ea75
avkanavina <a.kanavina@skyeng.ru>
2025-04-11 18:35:35 +0300

    Add lessons

--- courses/views.py
+++ courses/views.py
@@ -24,3 +24,4 @@ def course_detail(request, pk):
     course = get_object_or_404(Course, pk=pk)
     lessons = course.lessons.all()  # Получаем все уроки, связанные с курсом
     return render(request, 'courses/course_detail.html', {'course': course, 'lessons': lessons})
+
